4abb0931ae8d3ed4492c3f1fe1f577fa2a031c6f
Yasmine Ali <aliyasoo70@gmail.com>
2023-07-20 00:31:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ form.onsubmit = (e) => {
     e.preventDefault(); //preventing form from submitting
     statusTxt.style.display = 'block';
 
-   
+    let xhr = new XMLHttpRequest(); //creating new xml object
+    xhr.open("POST", "message.php", true); //sending post request to message.php file
+    xhr.onload = () => { //once ajax loaded
+        if (xhr.readyState == 4 && xhr.status == 200) {//if ajax response status is 200
+            let response = xhr.response; //storing ajax response
+            //if response is an error then change status color to red else reset the form after 3 seconds
+            if (response.indexOf('Email and password field is required') !== -1 || response.indexOf('Enter a valid email address') !== -1 || response.indexOf('Sorry, failed to send your message') !== -1) {
+                statusTxt.style.color = 'red';
+            } else {
+                form.reset();
+
+            }
+            statusTxt.innerText = response;
+        }
+    };
     let formData = new FormData(form); //creating new FormData obj. This obj is used to send form data
     xhr.send(formData);//sending form data to server
 }
